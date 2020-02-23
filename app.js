70199371e85db6f03e357a1bed52d9380f709679
Kindra Kinney <kindrakinney@gmail.com>
2020-02-22 21:08:37 -0800

    added function for prompts and async function

--- app.js
+++ app.js
@@ -109,3 +109,119 @@ function generateManagerHTML(manager) {
       </div>
     </div>`;
   } 
+
+  //----End variable that gets appended when the user is done adding employees------
+const end = `</div>
+</body>
+</html>`;
+
+//------The begining of the HTML page is generated first, then the intial prompt function is called.
+fs.writeFile("index.html", generateHTML, function (err) {
+if (err) {
+return console.log(err);
+}
+});
+init(); //Initial prompts
+
+//Initial prompt--------------------------------------------
+function promptUser() {
+return inquirer.prompt([
+{
+type: "input",
+name: "name",
+message: "What is your employees name?"
+},
+{
+type: "input",
+name: "id",
+message: "What is your employees id number?"
+},
+{
+type: "input",
+name: "email",
+message: "What is your employees email?"
+},
+{
+type: "rawlist",
+name: "role",
+choices: ["Manager", "Engineer", "Intern"]
+},
+])
+}
+
+
+//These functions are called based on what the users role selection is.
+var ManagerC = function () {
+return inquirer.prompt([
+{
+type: "input",
+name: "office",
+message: "What is the employees office number?"
+}
+])
+}
+
+var EngineerC = function () {
+return inquirer.prompt([
+{
+type: "input",
+name: "github",
+message: "What is your employees Github username?"
+}])
+}
+
+var InternC = function () {
+return inquirer.prompt([
+{
+type: "input",
+name: "school",
+message: "Where does the employee attend school?"
+},
+])
+}
+
+
+//------------------------------------------------------------
+async function init() { 
+try {
+const answers = await promptUser();
+if (answers.role == "Manager") {
+var officeNumber = await ManagerC(); //pauses the async function until the manager function executes 
+var newManager = new Manager(answers.name, answers.id, answers.email, officeNumber.office);
+var managerDiv = generateManagerHTML(newManager);
+await appendFileAsync("index.html", managerDiv);
+console.log("Added a Manager Card!")
+}
+else if (answers.role == "Engineer") {
+var getGithub = await EngineerC();
+var newEngineer = new Engineer(answers.name, answers.id, answers.email, getGithub.github);
+var engineerDiv = generateEngineerHTML(newEngineer);
+await appendFileAsync("index.html", (engineerDiv));
+console.log("Added an Engineer Card!")
+} else {
+var getSchool = await InternC();
+var newIntern = new Intern(answers.name, answers.id, answers.email, getSchool.school);
+var internDiv = generateInternHTML(newIntern);
+await appendFileAsync("index.html", (internDiv));
+console.log("Added an Intern Card!")
+}
+inquirer.prompt([
+{
+type: "rawlist",
+name: "employee",
+message: "Add another employee",
+choices: ["Yes", "No"]
+}
+]).then(
+function (addEmployee) {
+if (addEmployee.employee == "Yes") {
+init();
+} else {
+appendFileAsync("index.html", end);
+}
+}
+)
+} catch (err) {
+console.log(err);
+}
+}
